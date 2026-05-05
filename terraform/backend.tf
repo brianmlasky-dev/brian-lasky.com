c1235c50ba157d66f36f0d21d99dbf4af3938403
Brian Lasky <brian.lasky@outlook.com>
2026-05-05 16:27:12 +0000

    fix: terraform fmt backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "brian-lasky-terraform-state"
-    key            = "portfolio/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "brian-lasky-terraform-state"
+    key     = "portfolio/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
