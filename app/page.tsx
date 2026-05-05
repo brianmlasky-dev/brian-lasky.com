2c3da751fd5ad4ac8720f44096c3df4d52e1d5bd
Brian Lasky <brian.lasky@outlook.com>
2026-05-05 13:13:00 +0000

    feat: update page content and Next.js env types

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 'use client';
 
+import Navigation from './components/Navigation';
 import HeroSection from './components/HeroSection';
 import ArchitectureDiagram from './components/ArchitectureDiagram';
+import AboutSection from './components/AboutSection';
+import SkillsSection from './components/SkillsSection';
+import ProjectsSection from './components/ProjectsSection';
 import ContactForm from './components/ContactForm';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
     <main className="bg-gray-900">
+      <Navigation />
       <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
       <ArchitectureDiagram />
       <section id="contact" className="py-20 bg-gray-900">
         <ContactForm />
       </section>
+      <Footer />
     </main>
   );
 }
