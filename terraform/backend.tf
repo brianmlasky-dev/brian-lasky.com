d7b03211ece1f1b95eb7621fd6b0496cde9f25cf
Brian Lasky <brian.lasky@outlook.com>
2026-05-05 16:25:29 +0000

    fix: remove use_lockfile arg incompatible with Terraform 1.5.7

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "brian-lasky-terraform-state"
-    key          = "portfolio/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
+    bucket         = "brian-lasky-terraform-state"
+    key            = "portfolio/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
   }
 }
